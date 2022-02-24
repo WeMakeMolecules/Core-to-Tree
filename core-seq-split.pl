faf3e67603a5bb0ac759f3c2b6f6d50a477b8752
Pablo Cruz-Morales <68575424+pablo-genomes-to-vials-cruz@users.noreply.github.com>
2022-02-24 13:56:15 +0100

    Update core-seq-split.pl

--- core-seq-split.pl
+++ core-seq-split.pl
@@ -1,9 +1,9 @@
-print "# core_to_phylo.pl                                                               #\n";
+print "# Core_to_phylo.pl                                                               #\n";
 print "# A pipeline to make phylogenies from BPGA cores                                 #\n";
 print "# By Pablo Cruz-Morales                                                          #\n";
-print "# pcruzm@biosustain.dtu.dk     					                #\n";
-print "# usage: perl core_to_phylo.pl < BPGA core_seq.txt file> <BPGA DATASET.xls file> #\n\n\n";
-print "https\:\/\/github.com\/pablo-genomes-to-vials-cruz\/core_to_phylo.pl\/blob\/main\/core-seq-split.pl\n";
+print "# pcruzm\@biosustain.dtu.dk							#\n";
+print "# usage: perl core_to_phylo.pl < BPGA core_seq.txt file> <BPGA DATASET.xls file> #\n\n";
+print "https\:\/\/github.com\/pablo-genomes-to-vials-cruz\/core_to_phylo.pl\/blob\/main\/core-seq-split.pl\n\n";
 
 print "Eliminating files from previous runs\n";
 system 'rm  SUPER* 2> /dev/null';
@@ -14,7 +14,7 @@ system 'rm  FcC_smatrix.nex 2> /dev/null';
 system 'rm  *.htm 2> /dev/null';
 system 'rm  *listfaa 2> /dev/null';
 
-open FILE, $ARGV[0] or die "I cant read the inputfile\n";
+open FILE, $ARGV[0] or die "I cant read the core_seq.txt input file\n";
 open OUT, '>TEMP' or die "I cant save the  TEMP file\n";
 #----------making fasta oneline-------
 print "Formating the  input\n";
@@ -35,7 +35,7 @@ while ($line2=<TEMP>){
 	$fam=$1;
 	$org=$2;
 	$seq=$3;
-	open OUTFAM, ">>$fam.faa" or die "i cant print outfam file\n";
+	open OUTFAM, ">>$fam.faa" or die "I cant print outfam file\n";
 	print OUTFAM">$org\n$seq\n";
 }
 
@@ -43,11 +43,11 @@ system 'rm TEMP';
 close OUTFAM;
 system 'ls *.faa >listfaa';
 #------------------aligning-------------------
-print "Alingning a lot of sequences\n";
+print "Alingning a lot of sequences\n\n";
 open LIST, 'listfaa' or die "I cant open the listfaa\n";
 while ($listline=<LIST>){
 chomp $listline;
-print "Aligning sequences in protein family: $listline\n";
+print "Aligning sequences in protein family: $listline\t";
 system `muscle -in $listline -out $listline.aln -quiet`;
 #----trimming-----
 print "Trimming alignment for protein family: $listline \n";
@@ -56,10 +56,21 @@ system `mv $listline.aln-gb $listline.fas`;
 system `rm $listline.aln`;
 }
 
+#---some alignments are empty after trimming-----#
+#---eliminating empty alignents after trimming---#
+
+system 'grep "(0%" *.htm > EMPTYLIST';
+open EMPTY, 'EMPTYLIST' or die "I cant open the list of empty alignments\n";
+while ($empty=<EMPTY>){
+$empty=~/(\d+)(\.faa.+)/;
+print "No useful data left after trimming family $1, eliminating it from the matrix\n";
+system `rm $1\.faa.fas`;
+} 
 
 #--concatenating----
 #you neeed FasConCat for this step, get it here:
-#https://github.com/PatrickKueck/FASconCAT/blob/master/FASconCAT_v1.11.pl
+#https://github.com/PatrickKueck/FASconCAT/raw/master/FASconCAT_v1.11.pl
+
 print "Concatenating the trimmed aligments\n";
 system `perl FASconCAT_v1.11.pl -s -n`;
 #--cleaning-up----
@@ -69,14 +80,14 @@ system `rm listfaa`;
 system `mv FcC_smatrix.fas SUPER_MATRIX.txt`;
 
 #-----changing-indexes-to-species-names--#
-print "Naming the sequences in the comcatenated supermatrix\n";
+print "Naming the sequences in the concatenated supermatrix\n";
 open OUTMATRIX, '>SUPER_MATRIX.faa' or die "I cant write the SUPER_MATRIX.faa file\n";
 open SMATRIX, 'SUPER_MATRIX.txt' or die "I cant read the SUPER_MATRIX.txt file\n";
 while ($matline=<SMATRIX>){
 	if ($matline=~/>/){
 	$matline=~/(>)(\d+)/;
 	$ID="$2";
-	open DATASET, $ARGV[1] or die "I cant read the DATASET file\n";
+	open DATASET, $ARGV[1] or die "I cant read the DATASET.xls file\n";
 		while ($datline=<DATASET>){
 			if ($datline=~/Genome\_no\./){
 			$dummy++;
@@ -119,8 +130,8 @@ while ($catline=<CONCAT_FILE>){
         }
 }
 
-print "partitions file created! \n";
+print "Partitions file created! \n";
 #------Constructing the phylogeny with IQtree------#
-print "Building the phylogeny\n";
+print "Building the phylogeny\n\n";
 system 'iqtree -s SUPER_MATRIX.faa -spp SUPERMATRIX.partitions -m TEST -bb 10000';
 print "All done\n\n";
